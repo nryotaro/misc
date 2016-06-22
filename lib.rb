755f806b078b9653df839d3ace9da27637aa306d
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-06-22 11:34:55 +0900

    list libraries to create an assembled jar

---
+++ lib.rb
@@ -0,0 +1,6 @@
+#! /usr/bin/env ruby
+
+lib=`ls lib`.split(/\r?\n/)
+elems = lib.map{|item|  "<zipfileset excludes=\"META-INF/*.SF,META-INF/*.DSA,META-INF/*.RSA\" src=\"${lib.dir}/#{item}\"/>"  }
+
+elems.each{|e| puts e}
