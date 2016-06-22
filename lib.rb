563c90ca12bafe08a70881169771790e13107b57
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-06-22 11:56:06 +0900

    ignore sources, javadoc jar

--- lib.rb
+++ lib.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/env ruby
 
-lib=`ls lib`.split(/\r?\n/)
+lib=`ls lib`.split(/\r?\n/).reject{|e| e.match(/.+(javadoc|sources)\.jar$/)}
 elems = lib.map{|item|  "<zipfileset excludes=\"META-INF/*.SF,META-INF/*.DSA,META-INF/*.RSA\" src=\"${lib.dir}/#{item}\"/>"  }
 
 elems.each{|e| puts e}
